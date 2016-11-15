f25239933249fe2da57ea0c51b555fa67341fe2a
Mineev Alex <mineev.buryat@gmail.com>
2016-11-15 23:22:56 +0800

    django fix

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/django.py /etc/gunicorn.d/django.py
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+#sudo /etc/init.d/mysql start
